b4fc083053e957cf812ba32ca19ea9fc8cce97ab
BurningBright <lcg51271@gmail.com>
2016-08-19 18:15:03 +0800

    /** 2.3.11 */

--- src/class0203/ScanOverEqual.java
+++ src/class0203/ScanOverEqual.java
@@ -1,49 +1,61 @@
 package class0203;
+
+import java.util.Arrays;
+
+import stdlib.StdOut;
+import stdlib.StdRandom;
+
 /**
  * @Description 2.3.11
  *      快排跳过相等
+ *      感觉效率不是最高
  * @author Leon
  * @date 2016-08-18 16:09:47
  */
 public class ScanOverEqual {
     
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     private static void sort(Comparable[] a, int lo, int hi) { 
       if (hi <= lo) return;
       
-      int j = partition(a, lo, hi);
+      int i = lo, j = hi + 1;
+//      int lt = lo, gt = hi;
+      
+      Comparable v = a[lo];
+      while (true) { 
+          // 找比v大的元素
+          while(v.compareTo(a[++i]) >= 0)
+              if(i == hi) break;
+          // 找比v小的元素
+          while (v.compareTo(a[--j]) <= 0)
+              if (j == lo) break;
+          // ++i后成大组最小下标，--j后成小组最大下标
+          if(i >= j) break;
+//          StdOut.printf("%d\t%d\n", i, j);
+          
+          // 先整理两组
+          exch(a, i, j);
+          
+      }
+//      StdOut.println(Arrays.toString(a));
+      
+      // 整理相同元素
+      for(int k=hi; k>= i; k--) 
+          if(v.compareTo(a[k]) == 0)
+              exch(a, k, i++);
+      for(int k=lo+1; k<j+1; k++) 
+          if(v.compareTo(a[k]) == 0)
+              exch(a, k, j--);
+      
+      // put partitioning item v at a[j]
+      exch(a, lo, j);
+      
+      // now, a[lo .. j-1] < a[j] = a[i-1] < a[i .. hi]
+      
       sort(a, lo, j-1);
-      sort(a, j+1, hi);
+      sort(a, i, hi);
     }
     
-    @SuppressWarnings("rawtypes")
-    private static int partition(Comparable[] a, int lo, int hi) {
-        int i = lo;
-        int j = hi + 1;
-        Comparable v = a[lo];
-        while (true) { 
-            // find item on lo to swap
-            while (less(a[++i], v))
-                if (i == hi) break;
-            // find item on hi to swap
-            while (less(v, a[--j]))
-                if (j == lo) break;      // redundant since a[lo] acts as sentinel
-            
-            // check if pointers cross
-            if (i >= j) break;
-            exch(a, i, j);
-        }
-        // put partitioning item v at a[j]
-        exch(a, lo, j);
-        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
-        return j;
-    }
-    
- // is v < w ?
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    private static boolean less(Comparable v, Comparable w) {
-        return (v.compareTo(w) < 0);
-    }
-        
     // exchange a[i] and a[j]
     private static void exch(Object[] a, int i, int j) {
         Object swap = a[i];
@@ -51,8 +63,16 @@ public class ScanOverEqual {
         a[j] = swap;
     }
     
+    @SuppressWarnings("rawtypes")
     public static void main(String[] args) {
-        
+        int N = 50;
+        Comparable[] a = new Comparable[N];
+        for(int i=0; i<N; i++) {
+            a[i] = StdRandom.uniform(10);
+        }
+        StdOut.println(Arrays.toString(a));
+        sort(a, 0, a.length-1);
+        StdOut.println(Arrays.toString(a));
     }
 
 }
