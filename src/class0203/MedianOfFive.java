b74ce77c8522f9de4a536781fe133c66dc41a42a
BurningBright <lcg51271@gmail.com>
2016-09-08 16:45:40 +0800

    /** 2.3.19 */

---
+++ src/class0203/MedianOfFive.java
@@ -0,0 +1,93 @@
+package class0203;
+
+import java.util.Arrays;
+
+import stdlib.StdOut;
+import stdlib.StdRandom;
+
+/**
+ * @Description 
+ *      取5元素比较后取元素做标杆
+ *      样本空间应该要更大一些40->70
+ * @author Leon
+ * @date 2016-09-08 15:30:07
+ */
+public class MedianOfFive {
+
+    @SuppressWarnings("rawtypes")
+    private static void sort(Comparable[] a, int lo, int hi) { 
+        if (hi <= lo) return;
+        
+        int N = hi - lo + 1;
+        if(N <= 70) {
+            int m = median5(a, lo, lo+N/4, lo+N/2, lo+3*N/4, hi);
+            exch(a, m, lo);
+        }
+        
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private static int median5(Comparable[] a, int i, int j, int k, int m, int n) {
+        int t = (less(a[i], a[j]) ?
+                    (less(a[j], a[k]) ? j : less(a[i], a[k]) ? k : i) :
+                    (less(a[k], a[j]) ? j : less(a[k], a[i]) ? k : i));
+        return (less(a[t], a[m]) ?
+                    (less(a[m], a[n]) ? m : less(a[t], a[n]) ? n : t) :
+                    (less(a[n], a[m]) ? m : less(a[n], a[t]) ? n : t));
+    } 
+    
+    @SuppressWarnings("rawtypes")
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) { 
+
+            // find item on lo to swap
+            while (less(a[++i], v))
+                if (i == hi) break;
+
+            // find item on hi to swap
+            while (less(v, a[--j]))
+                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+
+            // check if pointers cross
+            if (i >= j) break;
+
+            exch(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+    
+ // is v < w ?
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private static boolean less(Comparable v, Comparable w) {
+        return (v.compareTo(w) < 0);
+    }
+        
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    
+    @SuppressWarnings("rawtypes")
+    public static void main(String[] args) {
+        Comparable[] a = new Comparable[50];
+        for(int i=0; i<a.length; i++)
+            a[i] = i;
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length-1);
+        StdOut.println(Arrays.toString(a));
+    }
+    
+}
---
+++ src/class0203/MedianOfThree.java
@@ -0,0 +1,91 @@
+package class0203;
+
+import java.util.Arrays;
+
+import stdlib.StdOut;
+import stdlib.StdRandom;
+
+/**
+ * @Description 2.3.18
+ *      取三个中中间的元素作为标杆，可以将样本分割得更均衡。
+ *      为什么要小于40时做三元素比较？
+ *      可能是大样本时混乱度(熵)高，选谁做标杆都差不多
+ * @author Leon
+ * @date 2016-08-31 11:05:25
+ */
+public class MedianOfThree {
+    
+    @SuppressWarnings("rawtypes")
+    private static void sort(Comparable[] a, int lo, int hi) { 
+        if (hi <= lo) return;
+        
+        int N = hi - lo + 1;
+        if(N <= 40) {
+            int m = median3(a, lo, lo+N/2, hi);
+            exch(a, m, lo);
+        }
+        
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private static int median3(Comparable[] a, int i, int j, int k) {
+        return (less(a[i], a[j]) ?
+               (less(a[j], a[k]) ? j : less(a[i], a[k]) ? k : i) :
+               (less(a[k], a[j]) ? j : less(a[k], a[i]) ? k : i));
+    } 
+    
+    @SuppressWarnings("rawtypes")
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) { 
+
+            // find item on lo to swap
+            while (less(a[++i], v))
+                if (i == hi) break;
+
+            // find item on hi to swap
+            while (less(v, a[--j]))
+                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+
+            // check if pointers cross
+            if (i >= j) break;
+
+            exch(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+    
+ // is v < w ?
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private static boolean less(Comparable v, Comparable w) {
+        return (v.compareTo(w) < 0);
+    }
+        
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    
+    @SuppressWarnings("rawtypes")
+    public static void main(String[] args) {
+        Comparable[] a = new Comparable[50];
+        for(int i=0; i<a.length; i++)
+            a[i] = i;
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length-1);
+        StdOut.println(Arrays.toString(a));
+    }
+    
+}
