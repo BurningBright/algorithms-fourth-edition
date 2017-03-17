2a333c23cda6438ca3e41e1f5f5c6bf3c39e95d0
BurningBright <lcg51271@gmail.com>
2017-03-17 18:34:12 +0800

    /** 3.2.6 */

--- src/class0302/HeightBST.java
+++ src/class0302/HeightBST.java
@@ -1,13 +1,15 @@
 package class0302;
 
-import class0302.BaseBST.Node;
 
 /**
  * @Description 3.2.6
+ *      computes the height of the tree
  * @author Leon
  * @date 2017-03-16 17:46:28
  */
 public class HeightBST<Key extends Comparable<Key>, Value> extends BaseBST<Key, Value> {
+    
+    private int height;
     
     private class HNode extends HeightBST.Node{
 //        private Key key;            // key
@@ -15,6 +17,7 @@ public class HeightBST<Key extends Comparable<Key>, Value> extends BaseBST<Key, 
 //        private HNode left, right;  // links to subtrees
 //        private int N;              // # nodes in subtree rooted here
         
+        @SuppressWarnings("unused")
         private int H;                // # node's tree height
 
         public HNode(Key key, Value val, int N, int H) {
@@ -27,31 +30,62 @@ public class HeightBST<Key extends Comparable<Key>, Value> extends BaseBST<Key, 
     public void put(Key key, Value val) {
         // Search for key. Update value if
         // found; grow table if new.
-        root = put(root, key, val);
+        root = put(root, key, val, 0);
+        
     }
 
-    private Node put(Node x, Key key, Value val) {
+    private Node put(Node x, Key key, Value val, int h) {
         // Change key’s value to val if key in subtree rooted at x.
         // Otherwise, add new node to subtree associating key with val.
-        if (x == null)
-            return new Node(key, val, 1);
+        if (x == null) {
+            // maintain the tree's height
+            if(h+1 > height)
+                height = h+1;
+            return new HNode(key, val, 1, ++h);
+        }
         int cmp = key.compareTo(x.key);
         if (cmp < 0)
-            x.left = put(x.left, key, val);
+            x.left = put(x.left, key, val, ++h);
         else if (cmp > 0)
-            x.right = put(x.right, key, val);
+            x.right = put(x.right, key, val, ++h);
         else
             x.val = val;
         x.N = size(x.left) + size(x.right) + 1;
         return x;
     }
     
+    public int height() {
+        return height(root);
+    }
     
+    private int height(Node x) {
+        if (x == null) return 0;
+        int left = 1+ height(x.left);
+//        StdOut.println(((HNode)x).H);
+        int right = 1 + height(x.right);
+        if(left > right)
+            return left;
+        else
+            return right;
+    }
+    
+    public int heightSize() {
+        return height;
+    }
     
     public static void main(String[] args) {
         HeightBST<String, String> bst = new HeightBST<String, String>();
-        bst.put("k1", "v1");
-        bst.get("k1");
+        bst.put("5", "v0");
+        bst.put("3", "v1");
+        bst.put("7", "v1");
+        bst.put("2", "v2");
+        bst.put("4", "v2");
+        bst.put("6", "v2");
+        bst.put("8", "v2");
+        bst.put("1", "v3");
+        
+        System.out.println(bst.height());
+        System.out.println(bst.heightSize());
     }
     
 }
