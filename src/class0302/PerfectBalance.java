439352aeb58e3ae9d29ad3a5d574c7ee990ef530
BurningBright <lcg51271@gmail.com>
2017-03-31 11:26:52 +0800

    /** 3.2.25 */

--- src/class0302/PerfectBalance.java
+++ src/class0302/PerfectBalance.java
@@ -1,11 +1,14 @@
 package class0302;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import stdlib.StdRandom;
 
 /**
  * @Description 3.2.25
+ *      equivalent to binary search
  * @author Leon
  * @date 2017-03-30 17:58:37
  */
@@ -22,7 +25,15 @@ public class PerfectBalance {
         if(lo == hi) 
             st.put(args[lo], "lv"+level);
         else if(lo < hi) {
-            int mid = lo + (hi - lo) / 2 + 1;
+            /*
+             * int mid = lo + (hi - lo) / 2 + 1;
+             * direct choose middle is not right
+             */
+            
+            int mid = calcMid(lo, hi);
+            
+//            System.out.println(mid);
+            
             st.put(args[mid], "lv"+level);
             
             // left
@@ -33,15 +44,41 @@ public class PerfectBalance {
         }
     }
     
-    public static void main(String[] args) {
-        int N = 10;
-        String[] src = new String[N];
-        for(int i=0; i<N; i++) 
-            src[i] = String.valueOf(StdRandom.uniform(100));
+    private static int calcMid (int lo, int hi) {
         
-        Arrays.sort(src);
+        if(hi-lo == 1)
+            return hi;
+        
+        int N = hi - lo + 1;
+        int n = (int)(Math.log(N+1)/Math.log(2));
+        int offset = (int) (N - (Math.pow(2.0, n) - 1));
+        offset = (int) (offset < Math.pow(2.0, n-1)? offset: Math.pow(2.0, n-1));
+        int mid = (int)(Math.pow(2.0, n-1) - 1) + offset;
+        
+        return lo + mid;
+    }
+    
+    public static void main(String[] args) {
+        int N = 26;
+        
+        Set<String> src = new HashSet<String>();
+        for(int i=0; i<N; i++) {
+            String tmp;
+            if(src.contains(tmp = String.valueOf(10 + StdRandom.uniform(90))))
+                i--;
+            else
+                src.add(tmp);
+        }
+        String[] srcX = src.toArray(new String[]{});
+        
+        /*
+        String[] srcX = new String[N];
+        for(int i=0; i<N; i++) 
+            srcX[i] = String.valueOf(10 + StdRandom.uniform(90));
+        */
+        Arrays.sort(srcX);
         Adjustable2DTree<String, String> a2t = new Adjustable2DTree<String, String>();
-        pb(a2t, src);
+        pb(a2t, srcX);
         
         a2t.calcTree();
     }
