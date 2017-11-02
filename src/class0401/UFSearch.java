d3ca08004b6e1019a2f9084a6c23b96ffca1cefe
BurningBright <lcg51271@gmail.com>
2017-11-02 18:01:43 +0800

    /** init chapter 4 */

---
+++ src/class0401/UFSearch.java
@@ -0,0 +1,25 @@
+package class0401;
+
+import rlgs4.Graph;
+
+/**
+ * @Description 4.1.18
+ *          联合查找搜索图实现
+ * @author Leon
+ * @date 2017-10-24 16:15:00
+ */
+public class UFSearch {
+    
+    UFSearch(Graph G, int s) {
+        
+    }
+    
+    boolean marked(int v) {
+        return false;
+    }
+    
+    int count() {
+        return 0;
+    }
+    
+}
